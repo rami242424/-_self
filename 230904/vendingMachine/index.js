b613650465f6d46d018628339eb3a6f7ee198d4d
Rami <138556019+rami242424@users.noreply.github.com>
2023-09-04 16:28:52 +0900

    Add files via upload

---
+++ 230904/vendingMachine/index.js
@@ -0,0 +1,8 @@
+import ColaGenerator from "./js/colaGenerator.js";
+
+const colaGenerator = new ColaGenerator();
+// console.log(await colaGenerator.loadData());
+// console.log();
+colaGenerator.setup();
+
+

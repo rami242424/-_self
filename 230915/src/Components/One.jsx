172d66562be5aa625c431ec34c267706f95a94e5
Rami <138556019+rami242424@users.noreply.github.com>
2023-09-15 23:02:35 +0900

    Add files via upload

---
+++ 230915/src/Components/One.jsx
@@ -0,0 +1,9 @@
+export default function One() {
+  return (
+    <div>
+      <h2>One</h2>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore, explicabo quam? Sit enim autem, dolor iure fugiat quis neque magnam consequuntur laborum incidunt labore reiciendis delectus? Placeat quasi deserunt numquam?</p>
+    </div>
+
+  )
+}

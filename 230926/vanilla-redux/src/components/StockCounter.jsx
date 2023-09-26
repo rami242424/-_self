e16a8a261818355a15a4aa0be13ed1c1d9a9336a
Rami <138556019+rami242424@users.noreply.github.com>
2023-09-26 17:26:08 +0900

    Update StockCounter.jsx

--- 230926/vanilla-redux/src/components/StockCounter.jsx
+++ 230926/vanilla-redux/src/components/StockCounter.jsx
@@ -1,14 +1,17 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { soldOut, sale } from "../modules/stockCounter";
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { soldOut, sale } from '../modules/stockCounter';
 
 export default function StockCounter() {
-    const {message} = useSelector((state)=> {
+
+    const { message } = useSelector((state) => {
         return {
             message: state.stockReducer.message
         }
     });
 
+
+    // store의 state 접근하도록 합니다.
     const { stock } = useSelector((state) => {
         return {
             stock: state.goodsReducer.stock
@@ -16,15 +19,15 @@ export default function StockCounter() {
     });
 
     const dispatch = useDispatch();
-    const onSoldout = () => dispatch(soldOut());
 
-    useEffect(()=> {
-        if(stock <= 0) {
-            onSoldout();
-        }else {
+    useEffect(() => {
+        if (stock <= 0) {
+            dispatch(soldOut());
+        } else {
             dispatch(sale());
         }
-    }, [stock])
+    }, [stock]);
+
 
     return (
         <p>{message}</p>

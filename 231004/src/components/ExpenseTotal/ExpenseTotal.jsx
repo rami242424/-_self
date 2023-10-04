69a7c036ffe81d00d87d7acca8626528ceadd204
Rami <138556019+rami242424@users.noreply.github.com>
2023-10-04 23:56:51 +0900

    Add files via upload

---
+++ 231004/src/components/ExpenseTotal/ExpenseTotal.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function ExpenseTotal() {
+  return (
+    <>
+        <div>
+            <span>총 지출 :</span>
+            <span>$ </span>
+        </div>
+        <div>
+            <span>잔액 :</span>
+            <span>$ </span>
+        </div>
+    </>
+  )
+}
